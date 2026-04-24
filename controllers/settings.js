88a93f2e227fe886128489f7431e6a01a6c8d9d6
احمد حسام محمد فرج <ahmdhsammfarge@gmail.com>
2026-04-24 03:18:38 +0300

    feat: settings routes

---
+++ controllers/settings.js
@@ -0,0 +1,22 @@
+async function updateSettings(req, res) {
+    // TODO
+}
+
+async function updateBlueprint(req, res) {
+    // TODO
+}
+
+async function resetRecords(req, res) {
+    // TODO
+}
+
+async function deleteAccount(req, res) {
+    // TODO
+}
+
+module.exports = {
+    updateSettings,
+    updateBlueprint,
+    resetRecords,
+    deleteAccount
+};
---
+++ routes/settings.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+const { updateSettings, updateBlueprint, resetRecords, deleteAccount} = require("../controllers/settings");
+
+const router = new Router();
+
+router.put("/settings", updateSettings);
+router.put("/blueprint", updateBlueprint);
+router.delete("/records", resetRecords);
+router.delete("/account", deleteAccount);
+
+module.exports = router;
